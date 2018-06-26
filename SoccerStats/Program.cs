577bf7c28fd5dca86693d0b3ee51daf63e54ace7
hvsang2101 <hvsang2101@gmail.com>
2018-06-26 14:09:06 -0700

    Add project files.

---
+++ SoccerStats/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoccerStats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var currentDirector = Directory.GetCurrentDirectory();
+            DirectoryInfo directoryInfo = new DirectoryInfo(currentDirector);
+            StreamReader
+        }
+    }
+}
